db56fb0030012cecf10b53ce7983b18c8797e61c
AnGal22 <antica.kantica22@gmail.com>
2025-12-28 02:15:14 +0100

    Added a responsive NavBar and some needed tools for the foundation of the site.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+
+import '../index.css'
+
+const Navbar = () => {
+
+    const [isVisible, setIsVisible] = useState(true)
+
+    useEffect(() => {
+        let prevScroll =  window.scrollY
+        const scrollNavBar = () => {
+            const currScroll=window.scrollY
+
+            if(currScroll > prevScroll || currScroll > 100){ //trenutni scroll mora biti veci od 100 piksela
+                setIsVisible(false)
+            } else {
+                setIsVisible(true)
+            }
+
+            prevScroll = currScroll
+    }
+
+      window.addEventListener("scroll", scrollNavBar)
+
+      return () => window.removeEventListener('scroll', scrollNavBar)
+    }, [])
+    
+
+
+
+
+  return (
+    <nav className={`fixed w-full bg-white shadow-md transition-transform duration-300 z-50 ${isVisible ? "translate-y-0" : "-translate-y-full"}`}>
+        <div className={`flex flex-wrap justify-around items-center`}>
+            <img src="logo.jpeg" alt="logo" className='w-35' />
+
+            <div className='flex flex-1 justify-around'>
+                <div>
+                    Home
+                </div>
+                <div>
+                    About
+                </div>
+                <div>
+                    Services
+                </div>
+                <div>
+                    Contact
+                </div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
